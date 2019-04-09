0ba79e7e7334af75aef9ac74ef16c6fa1421bef2
allanyang <allanyang@lexinfintech.com>
2019-04-09 09:51:03 +0800

    log restart

--- XCache/src/main/java/com/xuyang/work/cache/conf/XcacheConfig.java
+++ XCache/src/main/java/com/xuyang/work/cache/conf/XcacheConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.util.StringUtils;
 
+import java.util.Arrays;
+
 /**
  * @Auther: allanyang
  * @Date: 2019/4/8 21:18
@@ -30,9 +32,43 @@ public class XcacheConfig {
         }
 
         try {
+            //@todo 开关未必一直时开着的，这个要根据excluedEvnList来判断
+            property.setXcacheSwitch(true);
+            if (!StringUtils.isEmpty(env.getProperty(XcacheProperty.XCACHE_REDIS_KEY_PROPERTY))) {
+                property.setXcacheRedisKey(env.getProperty(XcacheProperty.XCACHE_REDIS_KEY_PROPERTY));
+            } else if (StringUtils.isEmpty(env.getProperty(XcacheProperty.DEFAULT_REDIS_KEY))) {
+                property.setXcacheRedisKey(env.getProperty(XcacheProperty.DEFAULT_REDIS_KEY));
+            } else {
+                throw new IllegalArgumentException(XcacheProperty.XCACHE_REDIS_KEY_PROPERTY + "of server.properties can not be empty");
+            }
 
+            if (!StringUtils.isEmpty(env.getProperty(XcacheProperty.XCACHE_TIME_PROPERTY))) {
+                property.setCacheSeconds(env.getProperty(XcacheProperty.XCACHE_TIME_PROPERTY, Integer.class));
+                if (property.getCacheSeconds() <= 0) {
+                    throw new IllegalArgumentException(XcacheProperty.XCACHE_TIME_PROPERTY + " of server.properties must be bigger than 0");
+                }
+            }
+
+            if (!StringUtils.isEmpty(env.getProperty(XcacheProperty.XCACHE_ALARM_TIME_PROPERTY))) {
+                property.setCacheAlarmSeconds(env.getProperty(XcacheProperty.XCACHE_ALARM_TIME_PROPERTY, Integer.class));
+                if (property.getCacheAlarmSeconds() <= 0) {
+                    throw new IllegalArgumentException(XcacheProperty.XCACHE_ALARM_TIME_PROPERTY + " of server.properties must bigger than 0");
+                }
+            }
+
+            if (!StringUtils.isEmpty(env.getProperty(XcacheProperty.XCACHE_LOCK_DEFAULT_TIMEOUT_PROPERTY))) {
+                property.setLockTimeoutMillisSeconds(env.getProperty(XcacheProperty.XCACHE_LOCK_DEFAULT_TIMEOUT_PROPERTY, Integer.class));
+                if (property.getLockTimeoutMillisSeconds() <= 0) {
+                    throw new IllegalArgumentException(XcacheProperty.XCACHE_LOCK_DEFAULT_TIMEOUT_PROPERTY + " of server.properties must bigger than 0");
+                }
+            }
+
+            if (StringUtils.isEmpty(XcacheProperty.XCACHE_LOCK_EXCLUDE_ENV)) {
+                property.setExcludeEnvList(Arrays.asList(StringUtils.split(env.getProperty(XcacheProperty.XCACHE_LOCK_EXCLUDE_ENV), ",")));
+            }
         } catch (Exception e) {
-           // log.warn("xcache config error", e);
+
+            //log.warn("xcache config error", e);
         }
 
         return property;
